7727f9adebf1667a80d7dd55d99612e591f2a40a
Bobo <yanglinbo@gmail.com>
2026-01-17 21:01:37 +0800

    refactor: rename audit.go to auditor.go and implement noop context and auditor

--- viewer/data_scope.go
+++ viewer/data_scope.go
@@ -3,5 +3,5 @@ package viewer
 // DataScope 定义数据权限范围
 type DataScope struct {
 	ScopeType string   // "department", "region", "all"
-	TargetIDs []uint32 // 具体的 ID 集合
+	TargetIDs []uint64 // 具体的 ID 集合
 }
