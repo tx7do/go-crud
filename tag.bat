3f4fc76dd85c55352881ef03eb702c15cf0fc12a
Bobo <yanglinbo@gmail.com>
2026-01-18 21:56:41 +0800

    feat: update pagination sorting logic to use Direction instead of Order and bump pagination version to v0.0.8

--- tag.bat
+++ tag.bat
@@ -3,13 +3,13 @@ git tag pagination/v0.0.8 --force
 git tag viewer/v0.0.5 --force
 git tag audit/v0.0.2 --force
 
-git tag entgo/v0.0.24 --force
-git tag gorm/v0.0.14 --force
+git tag entgo/v0.0.25 --force
+git tag gorm/v0.0.15 --force
 
 git tag cassandra/v0.0.3 --force
 git tag elasticsearch/v0.0.4 --force
-git tag clickhouse/v0.0.6 --force
-git tag influxdb/v0.0.6 --force
-git tag mongodb/v0.0.6 --force
+git tag clickhouse/v0.0.7 --force
+git tag influxdb/v0.0.7 --force
+git tag mongodb/v0.0.7 --force
 
 git push origin --tags
