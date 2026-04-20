425890fb81d565ef103871a5b6e74d97c16fce55
Bobo <yanglinbo@gmail.com>
2026-04-21 00:40:10 +0800

    feat: add OpenSearch client with CRUD, batch insert, ILM policy, index template, and query utilities; add tests and docs

--- tag.bat
+++ tag.bat
@@ -7,7 +7,7 @@ git tag entgo/v0.0.50 --force
 git tag gorm/v0.0.20 --force
 
 git tag cassandra/v0.0.3 --force
-git tag elasticsearch/v0.0.7 --force
+git tag elasticsearch/v0.0.8 --force
 git tag opensearch/v0.0.1 --force
 git tag clickhouse/v0.0.18 --force
 git tag influxdb/v0.0.12 --force
